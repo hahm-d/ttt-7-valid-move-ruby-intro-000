9d6b7e354fc5a8e50300344b6aa1ae380bd1a5b0
danny <ohdangitsdaniel@gmail.com>
2020-05-12 00:59:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
-
+def valid_move?(input)
+  if input.between?(0,8) && position_taken == false && input != nil
+    true
+  else 
+    false
+  end
+end  
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
